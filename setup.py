66de3f944783e3e78cc5afca1607a8013e629b5e
Talq8 <167497861+Talq8@users.noreply.github.com>
2024-06-04 11:21:32 +0000

    setup updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'isd',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Talal',
+    author_email= 'talnetflixq8@gmail.com',
     packages= find_packages(),
     install_requires = []
 
